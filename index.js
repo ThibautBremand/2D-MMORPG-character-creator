7cf46bfc72a1b0285f0c93101404f6c80dbfabd5
Thibaut BREMAND <thibaut.bremand@gmail.com>
2020-05-02 19:06:57 +0200

    Add map loader page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const serverURL = 'http://localhost:8080'
 const newCharacterRoute = serverURL + '/newcharacter'
+const newMapRoute = serverURL + '/newgamemap'
 
+// New characters
 function getUrlParams() {
     let inputVal = document.getElementById('name').value
     if (!inputVal || inputVal === '') {
@@ -8,12 +10,13 @@ function getUrlParams() {
         return
     }
     const pos = window.location.href.lastIndexOf('?')
+    let obj = {}
     if (pos < 0) {
-        sendJSON(newCharacterRoute, {Name: inputVal, Tiles: ''})
+        obj['body'] = 'light'
+        sendJSON(newCharacterRoute, {Name: inputVal, Props: JSON.stringify(obj)})
         return
     }
     let params = window.location.href.substring(pos + 1).split('&')
-    let obj = {}
     params = params.filter((elt) => {
         const splitted = elt.split('=')
         return splitted[0] && splitted[0] !== ''
@@ -23,9 +26,21 @@ function getUrlParams() {
         obj[splitted[0]] = splitted[1]
     })
 
-    sendJSON(newCharacterRoute, {Name: inputVal, Tiles: JSON.stringify(obj)})
+    sendJSON(newCharacterRoute, {Name: inputVal, Props: JSON.stringify(obj)})
 }
 
+// New gamemaps
+function handleMapInputs() {
+    let mapName = document.getElementById('mapname').value
+    if (!mapName || mapName === '') {
+        alert('You need to choose a name for your map!')
+        return
+    }
+
+    loadFile(mapName)
+}
+
+// Send JSON message to the server
 async function sendJSON(url, object) {
     fetch(url, {
         method: 'POST',
@@ -41,9 +56,35 @@ async function sendJSON(url, object) {
             return
         }
         if (response.status === 208) {
-            alert ('This username is already taken!')
+            alert ('This name is already taken!')
             return
         }
         alert ('The server encountered an error!')
     })
+}
+
+// Load the JSON gamemap file selected by the user, and send it to the server as JSON
+function loadFile(mapName) {
+    const input = document.getElementById('mapjson')
+    if (!input) {
+        alert('Um, couldn\'t find the fileinput element.')
+        return
+    }
+    if (!input.files) {
+        alert('This browser doesn\'t seem to support the `files` property of file inputs.')
+        return
+    }
+    if (!input.files[0]) {
+        alert('Please select a file')
+        return
+    }
+
+    const file = input.files[0]
+    const reader = new FileReader()
+    reader.onload = function(e) {
+        const arrayBuffer = new Uint8Array(reader.result)
+        const strJson = new TextDecoder('utf-8').decode(arrayBuffer)
+        sendJSON(newMapRoute, {Name: mapName, Props: strJson})
+    }
+    reader.readAsArrayBuffer(file)
 }
